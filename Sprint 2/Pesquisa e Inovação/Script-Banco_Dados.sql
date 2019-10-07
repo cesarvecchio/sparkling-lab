595b0c26f7a5d4a2ea5d7ce2536e28be7a157134
cesarvecchio <53615643+cesarvecchio@users.noreply.github.com>
2019-10-07 17:17:26 -0300

    Add files via upload

---
+++ Sprint 2/Pesquisa e Inovação/Script-Banco_Dados.sql
@@ -0,0 +1,72 @@
+create database Sistema_Poste;
+use Sistema_Poste;
+
+create table status_poste(
+	id_status_poste int primary key auto_increment,
+    descricao_status_poste varchar (50)
+);
+
+create table nivel_usuario(
+	id_nivel_usuario int primary key auto_increment,
+    descricao_nivel_usuario varchar (50)
+);
+
+create table poste(
+	id_poste int primary key auto_increment,
+    luminosidade varchar (50),
+    estado varchar (50),
+    cidade varchar (50),
+    bairro varchar (50),
+    logradouro varchar (60),
+    rua varchar (100),
+    cep char (9),
+    fk_status_poste int,
+    foreign key (fk_status_poste) references status_poste (id_status_poste)
+);
+
+create table admin(
+	id_admin int primary key auto_increment,
+    nome_admin varchar (100),
+	sobrenome_admin varchar (100),
+    email_admin varchar (100),
+    senha_admin varchar (100),
+    fk_nivel_usuario int,
+    foreign key (fk_nivel_usuario) references nivel_usuario (id_nivel_usuario)
+);
+
+create table usuario(
+	id_usuario int primary key auto_increment,
+    nome_usuario varchar (100),
+	sobrenome_usuario varchar (100),
+    email_usuario varchar (100),
+    senha_usuario varchar (100),
+    fk_nivel_usuario int,
+    foreign key (fk_nivel_usuario) references nivel_usuario (id_nivel_usuario)
+);
+
+
+create table funcionario(
+	id_funcionario int primary key auto_increment,
+    nome_funcionario varchar (100),
+	sobrenome_funcionario varchar (100),
+    email_funcionario varchar (100),
+    senha_funcionario varchar (100),
+    fk_nivel_usuario int,
+    foreign key (fk_nivel_usuario) references nivel_usuario (id_nivel_usuario)
+);
+
+
+
+
+insert into status_poste (descricao_status_poste) 
+	values ('Ativado'),
+		   ('Desativado'),
+           ('Mau funcionamento');
+           
+insert into nivel_usuario (descricao_nivel_usuario)
+	values ('Admin'),
+		   ('Funcionario'),
+           ('Usuario comum');
+           
+insert into admin (nome_admin, sobrenome_admin, email_admin, senha_admin, fk_nivel_usuario)
+	values ('Abacaxi', 'Dolly Citrus', 'abacaxi_citrus@gmail.com', 'dollynho', 1);
